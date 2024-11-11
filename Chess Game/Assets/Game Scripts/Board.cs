1b4f5cffd4040cdc7fae76b569f882ce330ed2b3
BrDakota <bdakota321@gmail.com>
2024-11-11 00:06:05 -0500

    Add C# files
    I added the main C# files (board, pieces, tiles). Made changes to class diagrams. I plan to iron out the logic of each class for play purposes before implementing game script.

---
+++ Chess Game/Assets/Game Scripts/Board.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Tiles[,] tilesArr = new Tiles[8, 8];
+    public Pieces[] piecesArr = new Pieces[32];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
