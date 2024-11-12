57e545167ea7ae84af336097a05d18e1ec4688ab
BrDakota <bdakota321@gmail.com>
2024-11-12 01:08:43 -0500

    Added Pieces
    I added the pieces to the board. They were also assigned into the array within the board script. The tiles array is now a 2d array that automatically finds the correct tiles for each index on start.
    
    I can begin to work on the logic for the game now that set up is complete.

--- Chess Game/Assets/Game Scripts/Board.cs
+++ Chess Game/Assets/Game Scripts/Board.cs
@@ -1,15 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public GameObject[] tilesArr;
+    public GameObject[,] tilesArr = new GameObject[8, 8];
     public GameObject[] piecesArr;
+
+    private string[] charArr = {"A", "B", "C", "D", "E", "F", "G", "H"};
 
     // Start is called before the first frame update
     void Start()
     {
+        // This loop fills the tiles array with all the correct tiles
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                tilesArr[i, j] = GameObject.Find($"{charArr[i]}{j + 1}");
+            }
+        }
     }
 
     // Update is called once per frame
